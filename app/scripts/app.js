031e12e24a7b2881706658461b83ef4ab955b0e1
VIGNESH K S <iamvvv0@gmail.com>
2021-11-06 22:01:15 +0530

    Initialize repository

---
+++ app/scripts/app.js
@@ -0,0 +1,49 @@
+document.onreadystatechange = function () {
+  if (document.readyState === 'interactive') renderApp();
+
+  function renderApp() {
+    var onInit = app.initialized();
+
+    onInit.then(getClient);
+    // .catch(handleErr);
+
+    function getClient(_client) {
+      window.client = _client;
+      client.events.on('app.activated', vm.init());
+    }
+  }
+};
+
+// function handleErr(err) {
+//   console.error(`Error occured. Details:`, err);
+// }
+
+const App = {
+  data() {
+    return {
+      messageTitle: '',
+      messageText: '',
+    };
+  },
+  methods: {
+    init() {
+      vm.getMessage();
+    },
+
+    getMessage() {
+      client.iparams.get().then(
+        function (data) {
+          vm.messageDate = data.message_date;
+          vm.messageTitle = data.message_title;
+          vm.messageText = data.message_text;
+        },
+        function (error) {
+          console.log(error);
+          // failure operation
+        }
+      );
+    },
+  },
+};
+
+const vm = Vue.createApp(App).mount('#app-body');
